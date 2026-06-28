3c449abf7d617211dea0842acdafcdcb1d1519fd
vennela227 <vennelav643@gmail.com>
2026-06-28 23:12:29 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll(".card button");
+
+buttons.forEach(button => {
+
+button.addEventListener("click", () => {
+
+alert("Product added to cart!");
+
+});
+
+});
